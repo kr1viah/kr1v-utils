b9f76d233232b2406161c7e8e5ece1aa9c23b5f2
kr1viah <kravpolet@gmail.com>
2025-11-02 16:41:54 +0100

    Init

---
+++ src/client/java/kr1v/kr1vUtils/client/Kr1vUtilsClient.java
@@ -0,0 +1,10 @@
+package kr1v.kr1vUtils.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Kr1vUtilsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
