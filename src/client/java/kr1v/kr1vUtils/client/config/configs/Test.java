5bf70996c60942bbd9602d67b6f7f7f7c7479ece
kr1viah <kravpolet@gmail.com>
2025-11-21 17:27:48 +0100

    More test configs

--- src/client/java/kr1v/kr1vUtils/client/config/configs/Test.java
+++ src/client/java/kr1v/kr1vUtils/client/config/configs/Test.java
@@ -7,10 +7,10 @@ import kr1v.kr1vUtils.client.utils.malilib.configbutton.ConfigButton;
 import kr1v.kr1vUtils.client.utils.malilib.plus.ConfigDoublePlus;
 import kr1v.kr1vUtils.client.utils.malilib.plus.ConfigIntegerPlus;
 
-@SuppressWarnings({"rawtypes", "unused"})
+@SuppressWarnings("rawtypes")
 @Config
 public class Test {
-    public static final ConfigButton CONFIG_BUTTON = new ConfigButton("Test button", "Press Me", () -> System.out.println("Test button pressed!"));
+    public static final ConfigButton CONFIG_BUTTON = new ConfigButton("Test button", "Press me", () -> System.out.println("Test button pressed!"));
 
     public static final Class<?> POPUP_DATA = PopupData.class;
     @PopupConfig(distanceFromSides = 50, distanceFromTops = 50)
@@ -18,11 +18,11 @@ public class Test {
         public static final ConfigInteger TEST = new ConfigIntegerPlus("Test", 0, -20, 20);
 
         public static final Class<?> LAYERED_CONFIGS = LayeredConfigs.class;
-        @PopupConfig
+        @PopupConfig(buttonName = "Custom button name")
         static class LayeredConfigs {
-            public static final ConfigDoublePlus DOUBLE = new ConfigDoublePlus("Hi", 0, -100, 100);
+            public static final ConfigDoublePlus DOUBLE = new ConfigDoublePlus("Double layered configs!", 0, -100, 100, "Haha, get it? because this is a double config?");
         }
     }
 
-    public static final ConfigButton CONFIG_BUTTON2 = new ConfigButton("Test button2", "Press Me", () -> System.out.println("Test button 2 pressed!"));
+    public static final ConfigButton CONFIG_BUTTON2 = new ConfigButton("Other test button", "Press me too!", () -> System.out.println("Test button 2 pressed!"));
 }
