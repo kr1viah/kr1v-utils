2d7bdf2708c44e8cdce1f4bf810d89abe29b06f8
kr1viah <kravpolet@gmail.com>
2025-12-16 20:37:23 +0100

    Update to new malilib api

--- src/client/java/kr1v/kr1vUtils/client/utils/Annotations.java
+++ src/client/java/kr1v/kr1vUtils/client/utils/Annotations.java
@@ -1,7 +1,7 @@
 package kr1v.kr1vUtils.client.utils;
 
 import kr1v.kr1vUtils.client.utils.annotation.classannotations.Touch;
-import kr1v.malilibApi.MalilibApi;
+import kr1v.malilibApi.InternalMalilibApi;
 
 import java.util.*;
 
@@ -9,7 +9,7 @@ public final class Annotations {
 	private Annotations() {}
 
 	static {
-        Set<Class<?>> touchableClasses = MalilibApi.reflections.getTypesAnnotatedWith(Touch.class);
+        Set<Class<?>> touchableClasses = InternalMalilibApi.reflections.getTypesAnnotatedWith(Touch.class);
         for (Class<?> cfgClass : touchableClasses) {
             Touch touch = cfgClass.getAnnotation(Touch.class);
             if (touch.async()) {
